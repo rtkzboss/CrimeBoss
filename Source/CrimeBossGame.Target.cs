f6fce51260534ff1fd9fa55817889a87ac0a7e7c
rtkz <154375853+rtkzboss@users.noreply.github.com>
2024-04-25 06:06:27 -0500

    Add primary game module

---
+++ Source/CrimeBossGame.Target.cs
@@ -0,0 +1,42 @@
+﻿using UnrealBuildTool;
+
+public class CrimeBossGameTarget : TargetRules {
+	public CrimeBossGameTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange(new string[] {
+			"BF_AI",
+			"BF_Analytics",
+			"BF_Animations",
+			"BF_Audio",
+			"BF_AudioBase",
+			"BF_Covers",
+			"BF_Debug",
+			"BF_FrameworkBase",
+			"BF_FrameworkGame",
+			"BF_GUI",
+			"BF_GUIBase",
+			"BF_Impacts",
+			"BF_Input",
+			"BF_LevelGenerator",
+			"BF_Loading",
+			"BF_Navigation",
+			"BF_Network",
+			"BF_Pool",
+			"BF_Props",
+			"BF_Rendering",
+			"BF_RideableVehicles",
+			"BF_Settings",
+			"BF_Voiceover",
+			"Common_Data",
+			"Common_Save",
+			"Common_Utils",
+			"CrimeBossMeta",
+			"IGS_UIFramework",
+			"Payback",
+			"PaybackDefinitions",
+			"PS5AudioFeatures",
+			"CrimeBoss",
+		});
+	}
+}
